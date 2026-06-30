5278305dd5b144e4399ac0212c0db8705f6a2610
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-06-30 13:49:23 +0530

    Time: 1 ms (99.92%), Space: 46.9 MB (45.38%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -11,13 +11,14 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] nums) {
         if(nums.length == 0) return null;
-        ListNode l1 = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            l1 = find(l1, nums[i]);
-        }
-        return l1;
+        return find(nums, 0, nums.length - 1);
     }
-    private ListNode find(ListNode l1, ListNode l2){
+    private ListNode find(ListNode[] nums, int i, int j){
+        if(i == j) return nums[i];
+        int mid = i + (j - i) / 2;
+        return merge(find(nums, i, mid), find(nums, mid + 1, j));
+    }
+    private ListNode merge(ListNode l1, ListNode l2){
         ListNode ans = new ListNode();
         ListNode val = ans;
         while(l1 != null && l2 != null){
@@ -30,8 +31,7 @@ class Solution {
             }
             ans = ans.next;
         }
-        if(l1 != null) ans.next = l1;
-        if(l2 != null) ans.next = l2;
+        ans.next = (l1 == null) ? l2 : l1;
         return val.next;
     }
 }
