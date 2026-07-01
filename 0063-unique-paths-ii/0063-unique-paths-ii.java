e2d1bbb69de1020e9a58f807cd087c3f119c278d
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-01 08:50:28 +0530

    Time: 0 ms (100%), Space: 43.3 MB (91.22%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,14 +3,14 @@ class Solution {
         int m = nums.length;
         int n = nums[0].length;
         if(nums[m - 1][n - 1] == 1) return 0;
-        int[][] dp = new int[m][n];
-        dp[m - 1][n - 1] = 1;
+        int[] dp = new int[n];
+        dp[n - 1] = 1;
         for(int i = m - 1; i >= 0; i--){
             for(int j = n - 1; j >= 0; j--){
-                if((i == m - 1 && j == n - 1) || nums[i][j] == 1) continue;
-                dp[i][j] = ((i + 1 < m) ? dp[i + 1][j] : 0) + ((j + 1 < n) ? dp[i][j + 1] : 0);
+                if(nums[i][j] == 1) dp[j] = 0;
+                else dp[j] += (j + 1 < n) ? dp[j + 1] : 0;
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 }
