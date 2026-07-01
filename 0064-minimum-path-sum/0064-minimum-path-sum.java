24852e560654413cabbf0ecdfb305eb4a970bb1c
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-01 08:59:39 +0530

    Time: 6 ms (10.85%), Space: 50.1 MB (84.34%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,18 +1,15 @@
 class Solution {
-    int m;
-    int n;
-    int[][] dp;
     public int minPathSum(int[][] nums) {
-        m = nums.length;
-        n = nums[0].length;
-        dp = new int[m][n];
-        for(int[] arr : dp) Arrays.fill(arr, -1);
-        return find(nums, 0, 0);
-    }
-    private int find(int[][] nums, int i, int j){
-        if(i == m - 1 && j == n - 1) return nums[i][j];
-        if(i >= m || j >= n) return Integer.MAX_VALUE;
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = nums[i][j] + Math.min(find(nums, i + 1, j), find(nums, i, j + 1));
+        int m = nums.length;
+        int n = nums[0].length;
+        int[][] dp = new int[m][n];
+        dp[m - 1][n - 1] = nums[m - 1][n - 1];
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i == m - 1 && j == n - 1) continue;
+                dp[i][j] = nums[i][j] + Math.min(((i + 1 < m) ? dp[i + 1][j] : Integer.MAX_VALUE), ((j + 1 < n) ? dp[i][j + 1] : Integer.MAX_VALUE));
+            }
+        }
+        return dp[0][0];
     }
 }
