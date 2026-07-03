bbf8d2f97801982eaccfce6748b97646609f0f1f
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-04 01:00:09 +0530

    Time: 9 ms (43.45%), Space: 41.8 MB (84.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,6 @@
 class Solution {
     public int fib(int n) {
-        return find(n);
-    }
-    private int find(int n){
         if(n == 0 || n == 1) return n;
-        return find(n - 1) + find(n - 2);
+        return fib(n - 1) + fib(n - 2);
     }
 }
