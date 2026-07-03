49f4c70ae9f26534767215c5e266d7dd7976f7d1
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-04 01:48:49 +0530

    Time: 0 ms (100%), Space: 42.8 MB (55.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,13 @@
 class Solution {
-    int n;
-    int[] dp;
     public int rob(int[] nums) {
-        n = nums.length;
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-        return find(nums, 0);
-    }
-    private int find(int[] nums, int i){
-        if(i >= n) return 0;
-        if(dp[i] != -1) return dp[i];
-        return dp[i] = Math.max(nums[i] + find(nums, i + 2), find(nums, i + 1));
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i = 2; i < n; i++){
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[n - 1];
     }
 }
