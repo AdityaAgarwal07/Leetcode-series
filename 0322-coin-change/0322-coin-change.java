a80f93bd4399a1d2ee753063c4c6603d8c7ba453
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-04 23:30:48 +0530

    Time: 11 ms (96.05%), Space: 46.4 MB (60.18%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,17 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int INF = 1000000;
-        int[][] dp = new int[n][amount + 1];
-        for (int j = 1; j <= amount; j++) {
-            if (j % coins[0] == 0)
-                dp[0][j] = j / coins[0];
-            else
-                dp[0][j] = INF;
+    public int coinChange(int[] nums, int t) {
+        int n = nums.length;
+        int inf = 1000000;
+        int[] dp = new int[t + 1];
+        for(int j = 1; j <= t; j++){
+            if(j % nums[0] == 0) dp[j] = j / nums[0];
+            else dp[j] = inf;
         }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (j >= coins[i]) {
-                    dp[i][j] = Math.min(dp[i][j], 1 + dp[i][j - coins[i]]);
-                }
+        for(int i = 1; i < n; i++){
+            for(int j = nums[i]; j <= t; j++){
+                dp[j] = Math.min(dp[j], 1 + dp[j - nums[i]]);
             }
         }
-        return dp[n - 1][amount] >= INF ? -1 : dp[n - 1][amount];
+        return dp[t] >= inf ? -1 : dp[t];
     }
 }
