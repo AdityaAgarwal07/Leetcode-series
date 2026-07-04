ee9891826d316a0bd803ab73a54ed6b7a98fbbaa
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-04 23:54:43 +0530

    Time: 11 ms (59.59%), Space: 57.3 MB (16.1%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,16 +1,17 @@
 class Solution {
-    int n;
-    int[][] dp;
     public int change(int t, int[] nums) {
-        n = nums.length;
-        dp = new int[n][t + 1];
-        for(int[] arr : dp) Arrays.fill(arr, -1);
-        return find(nums, 0, t);
-    }
-    private int find(int[] nums, int i, int t){
-        if(t == 0) return 1;
-        if(i == n || t < 0) return 0;
-        if(dp[i][t] != -1) return dp[i][t];
-        return dp[i][t] = find(nums, i, t - nums[i]) + find(nums, i + 1, t); 
+        int n = nums.length;
+        int[][] dp = new int[n][t + 1];
+        for(int i = 0; i < n; i++) dp[i][0] = 1;
+        for(int j = nums[0]; j <= t; j += nums[0]){
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j <= t; j++){
+                dp[i][j] = dp[i - 1][j];
+                if(j >= nums[i]) dp[i][j] += dp[i][j - nums[i]];
+            }
+        }
+        return dp[n - 1][t];
     }
 }
