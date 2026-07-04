1248ebba0ed4095a042340578273c0cf17fe17b0
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-04 23:56:14 +0530

    Time: 5 ms (99.88%), Space: 42.4 MB (99.38%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,17 +1,14 @@
 class Solution {
     public int change(int t, int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][t + 1];
-        for(int i = 0; i < n; i++) dp[i][0] = 1;
-        for(int j = nums[0]; j <= t; j += nums[0]){
-            dp[0][j] = 1;
-        }
+        int[] dp = new int[t + 1];
+        dp[0] = 1;
+        for(int j = nums[0]; j <= t; j += nums[0]) dp[j]++;
         for(int i = 1; i < n; i++){
-            for(int j = 1; j <= t; j++){
-                dp[i][j] = dp[i - 1][j];
-                if(j >= nums[i]) dp[i][j] += dp[i][j - nums[i]];
+            for(int j = nums[i]; j <= t; j++){
+                dp[j] += dp[j - nums[i]];
             }
         }
-        return dp[n - 1][t];
+        return dp[t];
     }
 }
