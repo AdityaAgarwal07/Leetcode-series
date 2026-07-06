3e93cc712dd2a3e00d3f7c119cf3c2e453a9fe90
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-06 09:48:44 +0530

    Time: 7 ms (10.11%), Space: 45.3 MB (5.51%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -1,20 +1,57 @@
 class Solution {
     public List<String> stringMatching(String[] nums) {
-        Arrays.sort(nums, (a, b) -> a.length() - b.length());
         List<String> ans = new ArrayList<>();
-        for(int i = 0; i < nums.length; i++){
-            String s1 = nums[i];
-            for(int j = i + 1; j < nums.length; j++){
-                if(i == j) continue;
-                if(find(s1, nums[j])){
-                    ans.add(s1);
+
+        Arrays.sort(nums, (a, b) -> a.length() - b.length());
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (kmp(nums[j], nums[i])) {
+                    ans.add(nums[i]);
                     break;
                 }
             }
         }
+
         return ans;
     }
-    private boolean find(String s1, String s2) {
-        return s2.contains(s1);
+
+    private boolean kmp(String text, String pattern) {
+        int[] lps = buildLPS(pattern);
+        int i = 0, j = 0;
+
+        while (i < text.length()) {
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+                if (j == pattern.length()) return true;
+            } else if (j > 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+
+        return false;
+    }
+
+    private int[] buildLPS(String pattern) {
+        int n = pattern.length();
+        int[] lps = new int[n];
+
+        int len = 0;
+        for (int i = 1; i < n;) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                lps[i++] = ++len;
+            } else if (len > 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
+            }
+        }
+
+        return lps;
     }
 }
