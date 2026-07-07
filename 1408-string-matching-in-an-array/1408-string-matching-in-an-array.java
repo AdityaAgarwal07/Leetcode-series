058f980e22eae3f3151e2940b576e0f024e20d3c
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-07 10:37:08 +0530

    Time: 4 ms (95.46%), Space: 44 MB (16.6%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -1,57 +1,19 @@
 class Solution {
     public List<String> stringMatching(String[] nums) {
-        List<String> ans = new ArrayList<>();
-
-        Arrays.sort(nums, (a, b) -> a.length() - b.length());
-
         int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (kmp(nums[j], nums[i])) {
+        Arrays.sort(nums, (a, b) -> a.length() - b.length());
+        List<String> ans = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(find(nums[i], nums[j])){
                     ans.add(nums[i]);
                     break;
                 }
             }
         }
-
         return ans;
     }
-
-    private boolean kmp(String text, String pattern) {
-        int[] lps = buildLPS(pattern);
-        int i = 0, j = 0;
-
-        while (i < text.length()) {
-            if (text.charAt(i) == pattern.charAt(j)) {
-                i++;
-                j++;
-                if (j == pattern.length()) return true;
-            } else if (j > 0) {
-                j = lps[j - 1];
-            } else {
-                i++;
-            }
-        }
-
-        return false;
-    }
-
-    private int[] buildLPS(String pattern) {
-        int n = pattern.length();
-        int[] lps = new int[n];
-
-        int len = 0;
-        for (int i = 1; i < n;) {
-            if (pattern.charAt(i) == pattern.charAt(len)) {
-                lps[i++] = ++len;
-            } else if (len > 0) {
-                len = lps[len - 1];
-            } else {
-                lps[i++] = 0;
-            }
-        }
-
-        return lps;
+    private boolean find(String s, String t){
+        return t.contains(s);
     }
 }
