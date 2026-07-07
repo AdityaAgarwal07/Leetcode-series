b3e5cf54fcaf6c601ed891aa774adcd9cde1863a
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-07 12:37:17 +0530

    Time: 7 ms (73.66%), Space: 49.7 MB (59.52%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,34 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] nums) {
-        List<List<String>> ans = new ArrayList<>();
-        List<String> a = new ArrayList<>();
-        a.add(nums[0]);
-        ans.add(a);
-        for(int i = 1; i < nums.length; i++){
-            boolean b = false;
-            for(List<String> ls : ans){
-                if(aa(ls.get(0), nums[i])){
-                    b = true;
-                    ls.add(nums[i]);
-                    break;
-                }
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : nums){
+            char[] cc = s.toCharArray();
+            Arrays.sort(cc);
+            String ss = String.valueOf(cc);
+            if(!map.containsKey(ss)){
+                map.put(ss, new ArrayList<>());
             }
-            if(b) continue;
-            List<String> ab = new ArrayList<>();
-            ab.add(nums[i]);
-            ans.add(ab);
+            map.get(ss).add(s);
         }
-        return ans;
-    }
-    private boolean aa(String s, String t){
-        if(s.length() != t.length()) return false;
-        int n = s.length();
-        int[] nums = new int[26];
-        for(int i = 0; i < n; i++){
-            nums[s.charAt(i) - 'a']++;
-            nums[t.charAt(i) - 'a']--;
-        }
-        for(int nn : nums) if(nn != 0) return false;
-        return true;
+        return new ArrayList<>(map.values());
     }
 }
