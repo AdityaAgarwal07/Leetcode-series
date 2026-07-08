b6f678318a5b2fb2b791d7c258417fe53d21a965
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-08 09:12:27 +0530

    Time: 1 ms (97.89%), Space: 42.8 MB (9.78%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,11 +1,6 @@
 class Solution {
     public int addDigits(int n) {
-        if(n / 10 == 0) return n;
-        int a = 0;
-        while(n > 0){
-            a += n % 10;
-            n /= 10;
-        }
-        return addDigits(a);
+        if (n == 0) return 0;
+        return 1 + (n - 1) % 9;
     }
 }
