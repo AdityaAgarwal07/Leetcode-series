1bd2ad8a0342dbc9db047bda993a1bdd2f4e81db
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-08 10:39:56 +0530

    Time: 0 ms (100%), Space: 44.1 MB (55.73%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] nums) {
+        for(int i = 0; i + 2 < nums.length; i++) if(nums[i] % 2 != 0 && nums[i + 1] % 2 != 0 && nums[i + 2] % 2 != 0) return true;
+        return false;
+    }
+}
