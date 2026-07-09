71753e5d3bc5cdcfe478555c42c530d397238c8d
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-10 00:31:57 +0530

    Time: 57 ms (16.44%), Space: 45.3 MB (88.37%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,34 +1,35 @@
 class Solution {
     int n;
     int m;
-    Boolean[][] dp;
+    Boolean[] dp;
     public boolean isMatch(String s, String t) {
         n = s.length();
         m = t.length();
-        dp = new Boolean[n + 1][m + 1];
-        dp[0][0] = true;
-        for(int i = 1; i <= n; i++){
-            dp[i][0] = false;
-        }
+        dp = new Boolean[m + 1];
+        dp[0] = true;
         int jj = 1;
         while(jj <= m && t.charAt(jj - 1) == '*'){
-            dp[0][jj] = true;
+            dp[jj] = true;
             jj++;
         }
         while(jj <= m){
-            dp[0][jj] = false;
+            dp[jj] = false;
             jj++;
         }
         for(int i = 1; i <= n; i++){
+            boolean p = dp[0];
+            dp[0] = false;
             for(int j = 1; j <= m; j++){
-                if(s.charAt(i - 1) == t.charAt(j - 1) || t.charAt(j - 1) == '?') dp[i][j] = dp[i - 1][j - 1];
+                boolean aa = dp[j];
+                if(s.charAt(i - 1) == t.charAt(j - 1) || t.charAt(j - 1) == '?') dp[j] = p;
                 else if(t.charAt(j - 1) == '*'){
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    dp[j] = dp[j] || dp[j - 1];
                 }else{
-                    dp[i][j] = false;
+                    dp[j] = false;
                 }
+                p = aa;
             }
         }
-        return dp[n][m];
+        return dp[m];
     }
 }
