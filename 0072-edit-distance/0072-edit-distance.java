6265516e35af0f0e1957ed5d23507c02bdcccd0d
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-09 22:45:07 +0530

    Time: 5 ms (68.87%), Space: 47.2 MB (34.39%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,21 +1,22 @@
 class Solution {
-    int n;
-    int m;
-    Integer[][] dp;
     public int minDistance(String s, String t) {
-        n = s.length();
-        m = t.length();
-        dp = new Integer[n][m];
-        return find(s, t, 0 , 0);
-    }
-    private int find(String s, String t, int i, int j){
-        if(i == n) return m - j;
-        if(j == m) return n - i;
-        if(dp[i][j] != null) return dp[i][j];
-        int aa = find(s, t, i + 1, j + 1);
-        if(s.charAt(i) == t.charAt(j)) return dp[i][j] = aa;
-        int a = find(s, t, i + 1, j);
-        int b = find(s, t, i , j + 1);
-        return dp[i][j] = 1 + Math.min(a, Math.min(aa, b));
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for(int j = 1; j <= m; j++) dp[0][j] = j;
+        for(int i = 1; i <= n; i++) dp[i][0] = i;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                int a = dp[i - 1][j - 1];
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    dp[i][j] = a;
+                }else{
+                    int b = dp[i - 1][j];
+                    int c = dp[i][j - 1];
+                    dp[i][j] = 1 + Math.min(a, Math.min(b, c));
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
