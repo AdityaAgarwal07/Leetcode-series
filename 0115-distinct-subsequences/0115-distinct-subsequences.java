432794303f2735e56cd8c131139eab69d2b04a8b
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-09 14:30:32 +0530

    Time: 27 ms (7.14%), Space: 54.6 MB (31.3%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -7,14 +7,15 @@ class Solution {
         m = t.length();
         dp = new int[n][m];
         for(int[] arr : dp) Arrays.fill(arr, -1);
-        return find(s, t, 0, 0 );
+        return find(s, t, 0, 0);
     }
     private int find(String s, String t, int i, int j){
         if(j == m) return 1;
         if(i == n) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        int a = 0;
+        int a = 0, b = 0;
         if(s.charAt(i) == t.charAt(j)) a = find(s, t, i + 1, j + 1);
-        return dp[i][j] = a + find(s, t, i + 1, j);
+        b = find(s, t, i + 1, j);
+        return dp[i][j] =  a + b;
     }
 }
