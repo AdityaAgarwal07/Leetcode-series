153f6de527a853caebdb6af2556f305de8172fa3
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-13 09:55:03 +0530

    Time: 52 ms (44.39%), Space: 117.8 MB (34.82%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -2,15 +2,14 @@ class Solution {
     public int maxProfit(int[] nums) {
         int n = nums.length;
         int[][][] dp = new int[n + 1][2][3];
-
         for(int i = n - 1; i >= 0; i--){
             for(int j = 0; j < 2; j++){
                 for(int k = 1; k <= 2; k++){
-                    if(j == 1) dp[i][j][k] = Math.max(dp[i + 1][0][k] - nums[i], dp[i + 1][j][k]);
-                    else dp[i][j][k] = Math.max(dp[i + 1][1][k - 1] + nums[i], dp[i + 1][j][k]);
+                    if(j == 0) dp[i][j][k] = Math.max(dp[i + 1][1][k] - nums[i], dp[i + 1][j][k]);
+                    else dp[i][j][k] = Math.max(dp[i + 1][0][k - 1] + nums[i], dp[i + 1][j][k]);
                 }
             }
         }
-        return dp[0][1][2];
+        return dp[0][0][2];
     }
 }
