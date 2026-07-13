4dd50f7b413145f1ad0a29b640e56caa02369b03
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-13 10:19:12 +0530

    Time: 2 ms (72.15%), Space: 45.1 MB (52.9%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,20 +1,15 @@
 class Solution {
-    int n;
-    int k;
-    Integer[][][] dp;
-    public int maxProfit(int K, int[] nums) {
-        n = nums.length;
-        k = K;
-        dp = new Integer[n][2][k];
-        return find(nums, 0, 0, true);
-    }
-    private int find(int[] nums, int i, int tt, boolean aa){
-        if(i == n || tt == k) return 0;
-        int t = (aa) ? 0 : 1;
-        if(dp[i][t][tt] != null) return dp[i][t][tt];
-        if(aa){
-            return dp[i][t][tt] = Math.max(find(nums, i + 1, tt, false) - nums[i], find(nums, i + 1, tt, aa));
+    public int maxProfit(int k, int[] nums) {
+        int n = nums.length;
+        int[][][] dp = new int[n + 1][2][k + 1];
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = 0; j < 2; j++){
+                for(int kk = 1; kk <= k; kk++){
+                    if(j == 0) dp[i][j][kk] = Math.max(dp[i + 1][1][kk] - nums[i], dp[i + 1][j][kk]);
+                    else dp[i][j][kk] = Math.max(dp[i + 1][0][kk - 1] + nums[i], dp[i + 1][j][kk]);
+                }
+            }
         }
-        return dp[i][t][tt] = Math.max(find(nums, i + 1, tt + 1, true) + nums[i], find(nums, i + 1, tt, aa));
+        return dp[0][0][k];
     }
 }
