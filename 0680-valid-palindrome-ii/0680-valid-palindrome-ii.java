e08b98d6ded8c07f5950f8d3631e49462cd5eff4
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-13 13:18:12 +0530

    Time: 4 ms (99.68%), Space: 47.6 MB (66.66%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -2,30 +2,21 @@ class Solution {
     public boolean validPalindrome(String s) {
         int n = s.length();
         int i = 0, j = n - 1;
-        while(i < j && s.charAt(i) == s.charAt(j)){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return find(s, i + 1, j) || find(s, i , j - 1);
+            }
             i++;
             j--;
         }
-        if(i == j) return true;
-        int i1 = i + 1, i2 = i, j1 = j, j2 = j - 1;
-        boolean a1 = true;
-        boolean a2 = true;
-        while(i1 < j1){
-            if(s.charAt(i1) != s.charAt(j1)){
-                a1 = false;
-                break;
-            }
-            i1++;
-            j1--;
+        return true;
+    }
+    private boolean find(String s, int i, int j){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
         }
-        while(i2 < j2){
-            if(s.charAt(i2) != s.charAt(j2)){
-                a2 = false;
-                break;
-            }
-            i2++;
-            j2--;
-        }
-        return a1 || a2;
+        return true;
     }
 }
