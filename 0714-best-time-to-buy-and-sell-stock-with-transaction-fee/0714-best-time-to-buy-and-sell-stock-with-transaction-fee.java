1aa6e89557bac3ce4943eef916a08b5faf4fb3d0
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-13 12:58:06 +0530

    Time: 4 ms (99.72%), Space: 65.4 MB (76.7%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,20 +1,13 @@
 class Solution {
-    int n;
-    int f;
-    Integer[][] dp;
-    public int maxProfit(int[] nums, int F) {
-        n = nums.length;
-        f = F;
-        dp = new Integer[n][2];
-        return find(nums, 0, true);
-    }
-    private int find(int[] nums, int i, boolean aa){
-        if(i == n) return 0;
-        int t = (aa) ? 0 : 1;
-        if(dp[i][t] != null) return dp[i][t];
-        if(aa){
-            return dp[i][t] = Math.max(find(nums, i + 1, false) - nums[i], find(nums, i + 1, aa));
+    public int maxProfit(int[] nums, int f) {
+        int n = nums.length;
+        int[] dp = new int[2];
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = 0; j < 2; j++){
+                if(j == 0) dp[j] = Math.max(dp[1] - nums[i], dp[j]);
+                else dp[j] = Math.max(dp[0] + nums[i] - f, dp[j]);
+            }
         }
-        return dp[i][t] = Math.max(find(nums, i + 1, true) + nums[i] - f, find(nums, i + 1, aa));
+        return dp[0];
     }
 }
