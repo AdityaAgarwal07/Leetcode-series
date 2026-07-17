0b0908cd3e5a0d89f8d01ba1fa883ded985cbfd9
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-17 23:33:59 +0530

    Time: 17 ms (63.76%), Space: 45.2 MB (57.96%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,16 +1,21 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        Arrays.sort(nums);
         int n = nums.length;
+        Arrays.sort(nums);
         int[] dp = new int[n];
+        int[] aa = new int[n];
         Arrays.fill(dp, 1);
-        dp[0] = 1;
+        aa[0] = 0;
         int ans = 1;
         int val = 0;
         for(int i = 1; i < n; i++){
-            for(int j = i - 1; j >= 0; j--){
+            aa[i] = i;
+            for(int j = 0; j < i; j++){
                 if(nums[i] % nums[j] == 0){
-                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                    if(dp[i] < 1 + dp[j]){
+                        dp[i] = 1 + dp[j];
+                        aa[i] = j;
+                    }
                 }
             }
             if(dp[i] > ans){
@@ -18,17 +23,12 @@ class Solution {
                 val = i;
             }
         }
-        List<Integer> aa = new ArrayList<>();
-        int p = val;
-        aa.add(nums[val]);
-        ans--;
-        for(int i = val - 1; i >= 0; i--){
-            if(ans == dp[i] && nums[p] % nums[i] == 0){
-                aa.add(0, nums[i]);
-                ans--;
-                p = i;
-            }
+        List<Integer> bb = new ArrayList<>();
+        while(aa[val] != val){
+            bb.add(0, nums[val]);
+            val = aa[val];
         }
-        return aa;
+        bb.add(0, nums[val]);
+        return bb;
     }
 }
