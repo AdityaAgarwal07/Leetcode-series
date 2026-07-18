09278ae31126f81cf1b39897461d082e64c49816
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-19 00:07:12 +0530

    Time: 19 ms (29.83%), Space: 46.9 MB (86.38%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,26 +1,32 @@
 class Solution {
-    int n, m;
-    Integer[][][] dp;
     public int cherryPickup(int[][] nums) {
-        n = nums.length;
-        m = nums[0].length;
-        dp = new Integer[m][n][m];
-        int ans = find(nums, 0, 0, 0);
-        return (ans < 0) ? 0 : ans;
-    }
-    private int find(int[][] nums, int j, int ii, int jj){
-        int i = ii + jj - j;
-        if(i < 0 || i >= n || j >= m || ii >= n || jj >= m) return Integer.MIN_VALUE;
-        if(nums[i][j] == -1 || nums[ii][jj] == -1) return Integer.MIN_VALUE;
-        if(i == n - 1 && j == m - 1 && ii == n - 1 && jj == m - 1) return nums[i][j];
-        if(dp[j][ii][jj] != null) return dp[j][ii][jj];
-        int a = find(nums,j, ii + 1, jj);
-        int b = find(nums,j + 1, ii, jj + 1);
-        int c = find(nums,j, ii, jj + 1);
-        int d = find(nums,j + 1, ii + 1, jj);
-        int e = Math.max(a, Math.max(b, Math.max(c, d)));
-        if(e == Integer.MIN_VALUE) return dp[j][ii][jj] = Integer.MIN_VALUE;
-        if(i == ii && j == jj) return dp[j][ii][jj] = nums[i][j] + e;
-        return dp[j][ii][jj] = nums[i][j] + nums[ii][jj] + e;
+        int n = nums.length;
+        int m = nums[0].length;
+        int[][][] dp = new int[m + 1][n + 1][m + 1];
+        for (int j = 0; j <= m; j++) {
+            for (int ii = 0; ii <= n; ii++) {
+                Arrays.fill(dp[j][ii], Integer.MIN_VALUE);
+            }
+        }
+        dp[m - 1][n - 1][m - 1] = nums[n - 1][m - 1];
+        for (int j = m - 1; j >= 0; j--) {
+            for (int ii = n - 1; ii >= 0; ii--) {
+                for (int jj = m - 1; jj >= 0; jj--) {
+                    int i = ii + jj - j;
+                    if (i < 0 || i >= n) continue;
+                    if (nums[i][j] == -1 || nums[ii][jj] == -1) continue;
+                    if (i == n - 1 && j == m - 1 && ii == n - 1 && jj == m - 1) continue;
+                    int a = dp[j][ii + 1][jj];
+                    int b = dp[j + 1][ii][jj + 1];
+                    int c = dp[j][ii][jj + 1];
+                    int d = dp[j + 1][ii + 1][jj];
+                    int e = Math.max(Math.max(a, b), Math.max(c, d));
+                    if (e == Integer.MIN_VALUE) continue;
+                    if (i == ii && j == jj) dp[j][ii][jj] = nums[i][j] + e;
+                    else dp[j][ii][jj] = nums[i][j] + nums[ii][jj] + e;
+                }
+            }
+        }
+        return Math.max(0, dp[0][0][0]);
     }
 }
