ad1c1e9bf61703765d9fbb4bf1289331322b1fd9
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-19 00:42:54 +0530

    Time: 32 ms (28%), Space: 48.1 MB (11.35%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,28 +1,39 @@
 class Solution {
     int[] aa = {-1, 0, 1};
     int n, m;
-    Integer[][][] dp;
+    int[][][] dp;
     public int cherryPickup(int[][] nums) {
         n = nums.length;
         m = nums[0].length;
-        dp = new Integer[n][m][m];
-        return find(nums, 0, 0, m - 1);
-    }
-    private int find(int[][] nums, int i, int j1, int j2){
-        if(j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) return Integer.MIN_VALUE;
-        if(i == n - 1){
-            if(j1 == j2) return nums[i][j1];
-            return nums[i][j1] + nums[i][j2];
-        }
-        if(dp[i][j1][j2] != null) return dp[i][j1][j2];
-        int ans = 0;
-        for(int n : aa){
-            for(int n1 : aa){
-                ans = Math.max(ans, find(nums, i + 1, j1 + n, j2 + n1));
+        dp = new int[n + 1][m + 2][m + 2];
+        for (int[][] arrr : dp) for (int[] arr : arrr) Arrays.fill(arr, Integer.MIN_VALUE);
+        dp[1][1][m] = nums[0][0];
+        if (m != 1) dp[1][1][m] += nums[0][m - 1];
+        for (int i = 2; i <= n; i++) {
+            for (int j1 = 1; j1 <= m; j1++) {
+                for (int j2 = 1; j2 <= m; j2++) {
+                    int ans = Integer.MIN_VALUE;
+                    for (int a : aa) {
+                        for (int b : aa) {
+                            int p1 = j1 - a;
+                            int p2 = j2 - b;
+                            if (p1 < 1 || p1 > m || p2 < 1 || p2 > m) continue;
+                            ans = Math.max(ans, dp[i - 1][p1][p2]);
+                        }
+                    }
+                    if (ans == Integer.MIN_VALUE) continue;
+                    int val = nums[i - 1][j1 - 1];
+                    if (j1 != j2) val += nums[i - 1][j2 - 1];
+                    dp[i][j1][j2] = ans + val;
+                }
             }
         }
-        if(ans == Integer.MIN_VALUE) return dp[i][j1][j2] = ans;
-        if(j1 == j2) return dp[i][j1][j2] = ans + nums[i][j1];
-        return dp[i][j1][j2] = ans + nums[i][j1] + nums[i][j2];
+        int ans = 0;
+        for (int j1 = 1; j1 <= m; j1++) {
+            for (int j2 = 1; j2 <= m; j2++) {
+                ans = Math.max(ans, dp[n][j1][j2]);
+            }
+        }
+        return ans;
     }
 }
