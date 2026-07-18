95ef0979600f2bf91aa0155d50b3699dd0d2c83e
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-18 13:47:51 +0530

    Time: 3 ms (54.99%), Space: 49 MB (12.34%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,19 +1,27 @@
 class Solution {
-    List<String> ans;
+    List<String>[] dp;
     public List<String> wordBreak(String s, List<String> nums) {
-        ans = new ArrayList<>();
-        find(s, nums, 0, "");
-        return ans;
+        dp = new ArrayList[s.length() + 1];
+        return find(s, nums, 0);
     }
-    private void find(String s, List<String> nums, int i, String val){
-        if(i == s.length()){
-            ans.add(val.trim());
-            return;
+    private List<String> find(String s, List<String> nums, int i) {
+        if (dp[i] != null) return dp[i];
+        List<String> ans = new ArrayList<>();
+        if (i == s.length()) {
+            ans.add("");
+            return dp[i] = ans;
         }
-        for(String ss : nums){
-            if(i + ss.length() <= s.length() && s.startsWith(ss, i)){
-                find(s, nums, i + ss.length(), val + " " + ss);
+        for (String ss : nums) {
+            if (i + ss.length() <= s.length() && s.startsWith(ss, i)) {
+                List<String> curr = find(s, nums, i + ss.length());
+                for (String str : curr) {
+                    if (str.length() == 0)
+                        ans.add(ss);
+                    else
+                        ans.add(ss + " " + str);
+                }
             }
         }
+        return dp[i] = ans;
     }
 }
