d70aeaef4b502adf09d7de2273f3c921081aceed
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-18 14:14:09 +0530

    Time: 3 ms (54.99%), Space: 48.8 MB (20.17%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,27 +1,35 @@
 class Solution {
-    List<String>[] dp;
+    List<String> ans;
     public List<String> wordBreak(String s, List<String> nums) {
-        dp = new ArrayList[s.length() + 1];
-        return find(s, nums, 0);
-    }
-    private List<String> find(String s, List<String> nums, int i) {
-        if (dp[i] != null) return dp[i];
-        List<String> ans = new ArrayList<>();
-        if (i == s.length()) {
-            ans.add("");
-            return dp[i] = ans;
-        }
-        for (String ss : nums) {
-            if (i + ss.length() <= s.length() && s.startsWith(ss, i)) {
-                List<String> curr = find(s, nums, i + ss.length());
-                for (String str : curr) {
-                    if (str.length() == 0)
-                        ans.add(ss);
-                    else
-                        ans.add(ss + " " + str);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[n] = true;
+        for(int i = n - 1; i >= 0; i--){
+            for(String ss : nums){
+                if(i + ss.length() <= s.length() && s.startsWith(ss, i)){
+                    if(dp[i + ss.length()]){
+                        dp[i] = true;
+                        break;
+                    }
                 }
             }
         }
-        return dp[i] = ans;
+        if(!dp[0]) return new ArrayList<>();
+        ans = new ArrayList<>();
+        find(dp, s, nums, 0, "");
+        return ans;
+    }
+    private void find(boolean[] dp, String s, List<String> nums, int i, String val){
+        if(i == s.length()){
+            ans.add(val.trim());
+            return;
+        }
+        for(String ss : nums){
+            if(i + ss.length() <= s.length()){
+                if(s.startsWith(ss, i)){
+                    if(dp[i + ss.length()]) find(dp, s, nums, i + ss.length(), val + " " + ss);
+                }
+            }
+        }
     }
 }
