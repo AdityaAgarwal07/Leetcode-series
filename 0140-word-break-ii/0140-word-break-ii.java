4ca575f4f3a9d6b03b38b0e0d3051cd9b73c0ad9
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-18 14:23:28 +0530

    Time: 1 ms (94.41%), Space: 43.1 MB (87.6%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -4,31 +4,33 @@ class Solution {
         int n = s.length();
         boolean[] dp = new boolean[n + 1];
         dp[n] = true;
-        for(int i = n - 1; i >= 0; i--){
-            for(String ss : nums){
-                if(i + ss.length() <= s.length() && s.startsWith(ss, i)){
-                    if(dp[i + ss.length()]){
-                        dp[i] = true;
-                        break;
-                    }
+        for (int i = n - 1; i >= 0; i--) {
+            for (String ss : nums) {
+                if (i + ss.length() <= n &&
+                    s.startsWith(ss, i) &&
+                    dp[i + ss.length()]) {
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-        if(!dp[0]) return new ArrayList<>();
+        if (!dp[0]) return new ArrayList<>();
         ans = new ArrayList<>();
-        find(dp, s, nums, 0, "");
+        find(dp, s, nums, 0, new StringBuilder());
         return ans;
     }
-    private void find(boolean[] dp, String s, List<String> nums, int i, String val){
-        if(i == s.length()){
-            ans.add(val.trim());
+    private void find(boolean[] dp, String s, List<String> nums, int i, StringBuilder val) {
+        if (i == s.length()) {
+            ans.add(val.toString());
             return;
         }
-        for(String ss : nums){
-            if(i + ss.length() <= s.length()){
-                if(s.startsWith(ss, i)){
-                    if(dp[i + ss.length()]) find(dp, s, nums, i + ss.length(), val + " " + ss);
-                }
+        for (String ss : nums) {
+            if (i + ss.length() <= s.length() && s.startsWith(ss, i) && dp[i + ss.length()]) {
+                int len = val.length();
+                if (len != 0) val.append(" ");
+                val.append(ss);
+                find(dp, s, nums, i + ss.length(), val);
+                val.setLength(len);
             }
         }
     }
