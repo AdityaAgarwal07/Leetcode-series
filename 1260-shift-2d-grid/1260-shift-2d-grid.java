9b5fd0785f7862aa9c438b4689237247715795ad
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-20 15:26:26 +0530

    Time: 6 ms (55.4%), Space: 47.2 MB (44.14%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -2,32 +2,17 @@ class Solution {
     public List<List<Integer>> shiftGrid(int[][] nums, int k) {
         int n = nums.length;
         int m = nums[0].length;
-        k = k % (n * m);
-        int val = (n * m) - k;
+        int s = n * m;
+        k %= s;
+        int start = (s - k) % s;
         List<Integer> aa = new ArrayList<>();
-        for(int[] arr : nums) for(int nn : arr) aa.add(nn);
+        for (int[] arr : nums) for (int nn : arr) aa.add(nn);
         List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0; i < n; i++) ans.add(new ArrayList<>());
-        int mm = val;
-        int i = 0, j = 0;
-        while(val < aa.size()){
-            ans.get(i).add(aa.get(val));
-            j++;
-            val++;
-            if(j == m){
-                j = 0;
-                i++;
-            }
+        for (int i = 0; i < n; i++) {
+            ans.add(new ArrayList<>());
         }
-        val = 0;
-        while(val < mm){
-            ans.get(i).add(aa.get(val));
-            j++;
-            val++;
-            if(j == m){
-                j = 0;
-                i++;
-            }
+        for (int i = 0; i < s; i++) {
+            ans.get(i / m).add(aa.get((start + i) % s));
         }
         return ans;
     }
