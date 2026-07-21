4764540895cf490349733dc96d32697a5c88ad38
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-22 01:58:39 +0530

    Time: 2 ms (41.29%), Space: 53.6 MB (15.33%) - LeetHub

---
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public final TreeNode getTargetCopy(final TreeNode a, final TreeNode b, final TreeNode t) {
+        return find(a, b, t);
+    }
+    private TreeNode find(TreeNode a, TreeNode b, TreeNode t){
+        if(a.val == t.val) return b;
+        TreeNode aa = (a.left != null) ? find(a.left, b.left, t) : null;
+        TreeNode bb = (a.right != null) ? find(a.right, b. right, t) : null;
+        return (aa == null) ? bb : aa;
+    }
+}
