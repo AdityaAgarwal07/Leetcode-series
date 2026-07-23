bf6e51a4242b799e3fe229e08f1de0b05e06f089
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-23 12:26:51 +0530

    Time: 120 ms (17.52%), Space: 46.8 MB (5.84%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -0,0 +1,22 @@
+class Solution {
+    Integer[][] dp;
+    public int maxCoins(int[] aa) {
+        int n = aa.length;
+        dp = new Integer[n + 2][n + 2];
+        int[] nums = new int[n + 2];
+        nums[0] = 1;
+        for(int i = 0; i < n; i++) nums[i + 1] = aa[i];
+        nums[n + 1] = 1;
+        return find(nums, 0, n + 1);
+    }
+    private int find(int[] nums, int i, int j){
+        if(j - i <= 1) return 0;
+        if(dp[i][j] != null) return dp[i][j];
+        int ans = Integer.MIN_VALUE;
+        for(int k = i + 1; k < j; k++){
+            int val = (nums[k] * nums[i] * nums[j]) + find(nums, i, k) + find(nums, k, j);
+            ans = Math.max(ans, val);
+        }
+        return dp[i][j] = ans;
+    }
+}
