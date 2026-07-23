2eca4153706db2ae381cb805805842c65f66669a
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-23 11:52:58 +0530

    Time: 14 ms (95.42%), Space: 44.3 MB (75.43%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,25 +1,35 @@
 class Solution {
-    Integer[][] dp;
+    int[][] dp;
     public int minCost(int nn, int[] c) {
         int n = c.length;
         Arrays.sort(c);
         int[] nums = new int[n + 2];
         nums[0] = 0;
-        dp = new Integer[n + 1][n + 1];
+        dp = new int[n + 2][n + 2];
         for(int i = 0; i < n; i++){
             nums[i + 1] = c[i];
         }
         nums[n + 1] = nn;
-        return find(nums, 1, n);
-    }
-    private int find(int[] nums, int i, int j){
-        if(i > j) return 0;
-        if(dp[i][j] != null) return dp[i][j];
-        int ans = Integer.MAX_VALUE;
-        for(int k = i; k <= j; k++){
-            int val = nums[j + 1] - nums[i - 1] + find(nums, i, k - 1) + find(nums, k + 1, j);
-            ans = Math.min(ans, val);
+        for(int i = n; i >= 1; i--){
+            for(int j = i; j <= n; j++){
+                int ans = Integer.MAX_VALUE;
+                for(int k = i; k <= j; k++){
+                    int val = nums[j + 1] - nums[i - 1] + dp[i][k - 1] + dp[k + 1][j];
+                    ans = Math.min(ans, val);
+                }
+                dp[i][j] = ans;
+            }
         }
-        return dp[i][j] = ans;
+        return dp[1][n];
     }
+    // private int find(int[] nums, int i, int j){
+    //     if(i > j) return 0;
+    //     if(dp[i][j] != null) return dp[i][j];
+    //     int ans = Integer.MAX_VALUE;
+    //     for(int k = i; k <= j; k++){
+    //         int val = nums[j + 1] - nums[i - 1] + find(nums, i, k - 1) + find(nums, k + 1, j);
+    //         ans = Math.min(ans, val);
+    //     }
+    //     return dp[i][j] = ans;
+    // }
 }
