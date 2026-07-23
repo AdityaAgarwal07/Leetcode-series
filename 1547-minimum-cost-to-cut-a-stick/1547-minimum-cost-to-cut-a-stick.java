413d0877fa2450af7c6b9fc23fa12b0282b50ad3
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-23 11:48:12 +0530

    Time: 26 ms (42.71%), Space: 46.5 MB (5.07%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,34 +1,25 @@
 class Solution {
-    int n;
-    int[][] dp;
-    public int minCost(int nn, int[] cuts) {
-        n = cuts.length;
+    Integer[][] dp;
+    public int minCost(int nn, int[] c) {
+        int n = c.length;
+        Arrays.sort(c);
         int[] nums = new int[n + 2];
-        nums[n] = 0;
-        nums[n + 1] = nn;
-        for(int i = 0; i < n; i++) nums[i] = cuts[i];
-        dp = new int[n + 2][n + 2];
-        Arrays.sort(nums);
-        for(int i = n; i >= 1; i--){
-            for(int j = i; j <= n; j++){
-                int ans = (int) 1e9;
-                for(int k = i; k <= j; k++){
-                    int val = nums[j + 1] - nums[i - 1] + dp[i][k - 1] + dp[k + 1][j];
-                    ans = Math.min(ans, val);
-                }
-                dp[i][j] = ans;
-            }
+        nums[0] = 0;
+        dp = new Integer[n + 1][n + 1];
+        for(int i = 0; i < n; i++){
+            nums[i + 1] = c[i];
         }
-        return dp[1][n];
+        nums[n + 1] = nn;
+        return find(nums, 1, n);
     }
-    // private int find(int[] nums, int i, int j){
-    //     if(i > j) return 0;
-    //     if(dp[i][j] != null) return dp[i][j];
-    //     int ans = (int) 1e9;
-    //     for(int k = i; k <= j; k++){
-    //         int val = nums[j + 1] - nums[i - 1] + find(nums, i, k - 1) + find(nums, k + 1, j);
-    //         ans = Math.min(ans, val);
-    //     }
-    //     return dp[i][j] = ans;
-    // }
+    private int find(int[] nums, int i, int j){
+        if(i > j) return 0;
+        if(dp[i][j] != null) return dp[i][j];
+        int ans = Integer.MAX_VALUE;
+        for(int k = i; k <= j; k++){
+            int val = nums[j + 1] - nums[i - 1] + find(nums, i, k - 1) + find(nums, k + 1, j);
+            ans = Math.min(ans, val);
+        }
+        return dp[i][j] = ans;
+    }
 }
