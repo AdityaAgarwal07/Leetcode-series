644953c0c99df03e17005531f7e5e158840ba438
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-07-31 01:24:12 +0530

    Time: 0 ms (100%), Space: 43.1 MB (79.47%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minimumPushes(String s) {
+        int n = s.length(), i = 0, val = 1;
+        int ans = 0;
+        while(i < n){
+            if(i + 7 < n){
+                ans += val * 8;
+                val++;
+                i += 8;
+            }else{
+                ans += val;
+                i++;
+            }
+        }
+        return ans;
+    }
+}
