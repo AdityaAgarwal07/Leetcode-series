095ff797d7770b6f9fc9cab36edd5f5763de8da1
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-08-25 09:48:42 +0530

    Time: 2 ms (37.26%), Space: 46.7 MB (98.36%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -3,19 +3,24 @@ class Solution {
         int n = nums.length;
         int[] arr = new int[n];
         Arrays.fill(arr, -1);
+
         for(int i = 0; i < n; i++){
             if(arr[i] != -1) continue;
-            Stack<Integer> q = new Stack<>();
+
+            Deque<Integer> q = new ArrayDeque<>();
             q.push(i);
             arr[i] = 0;
+
             while(!q.isEmpty()){
                 int nn = q.pop();
                 int a = arr[nn];
+
                 for(int m : nums[nn]){
                     if(arr[m] != -1){
                         if(arr[m] == a) return false;
                         continue;
                     }
+
                     int b = (a == 0) ? 1 : 0;
                     arr[m] = b;
                     q.push(m);
@@ -23,6 +28,6 @@ class Solution {
             }
         }
 
-        return true; 
+        return true;
     }
 }
