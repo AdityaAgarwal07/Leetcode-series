53f13140b2fdc97ac7583970ddadd95224a0f79d
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-08-28 10:22:26 +0530

    Time: 74 ms (62.79%), Space: 47.8 MB (67.22%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,37 @@
+class Solution {
+    class Pair{
+        String ss;
+        int nn;
+        Pair(String ss, int nn){
+            this.ss = ss;
+            this.nn = nn;
+        }
+    }
+    public int ladderLength(String ttt, String t, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
+        if(!set.contains(t)) return 0;
+        Queue<Pair> q = new ArrayDeque<>();
+        q.offer(new Pair(ttt, 1));
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            String s = p.ss;
+            int n = p.nn;
+            if(t.equals(s)) return n;
+            StringBuilder sb = new StringBuilder(s);
+            for(int i = 0; i < s.length(); i++){
+                char cc = s.charAt(i);
+                for(int j = 0; j < 26; j++){
+                    char c = (char) (j + 'a');
+                    sb.setCharAt(i, c);
+                    String a = sb.toString();
+                    if(set.contains(a)){
+                        set.remove(a);
+                        q.offer(new Pair(a, n + 1));
+                    }
+                }
+                sb.setCharAt(i, cc);
+            }
+        }
+        return 0;
+    }
+}
