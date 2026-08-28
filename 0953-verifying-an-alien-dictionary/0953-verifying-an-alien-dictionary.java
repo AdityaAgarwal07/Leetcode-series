39e9097af306211c375c620ecde45e2f1172510b
Aditya Agarwal <aadiagarwal2024@gmail.com>
2026-08-28 09:55:02 +0530

    Time: 0 ms (100%), Space: 43.4 MB (75.67%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.java
@@ -1,30 +1,23 @@
 class Solution {
-    public boolean isAlienSorted(String[] words, String o) {
-        int n = words.length;
-        List<Character>[] nums = new ArrayList[26];
-        for(int i = 0; i < 26; i++) nums[i] = new ArrayList<>();
-        for(int i = 0; i < n - 1; i++){
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] rank = new int[26];
+        for (int i = 0; i < 26; i++) {
+            rank[order.charAt(i) - 'a'] = i;
+        }
+        for (int i = 0; i < words.length - 1; i++) {
             String s1 = words[i];
             String s2 = words[i + 1];
             int j = 0;
-            while (j < s1.length() && j < s2.length() && s1.charAt(j) == s2.charAt(j)){
+            while (j < s1.length() && j < s2.length()
+                    && s1.charAt(j) == s2.charAt(j)) {
                 j++;
             }
-            if (j == s2.length() && j < s1.length()) return false;
-            if (j < s1.length() && j < s2.length()) {
-                char c1 = s1.charAt(j);
-                char c2 = s2.charAt(j);
-                nums[c1 - 'a'].add(c2);
+            if (j == s2.length() && j < s1.length()) {
+                return false;
             }
-        }
-        for(int i = 0; i < 26; i++){
-            if(nums[i].size() != 0){
-                char c = (char) (i + 'a');
-                int nn = o.indexOf(c);
-                for(char cc : nums[i]){
-                    int mm = o.indexOf(cc);
-                    if(nn > mm) return false;
-                }
+            if (j < s1.length() && j < s2.length()
+                    && rank[s1.charAt(j) - 'a'] > rank[s2.charAt(j) - 'a']) {
+                return false;
             }
         }
         return true;
